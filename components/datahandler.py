ccbb2d2aac063c1a2bdbd81558d401df75755a9c
Ali Mosallaei <alimos@umich.edu>
2024-05-06 16:28:11 -0400

    Added more capability to datahandler

--- components/datahandler.py
+++ components/datahandler.py
@@ -7,24 +7,26 @@ class DataHandler:
         self.network = network
         
     # NOTE: path1 is determined as the reconfigurable path if plotting meaningfuls    
-    def plot_trp_comparison(self, path1, path2, path1_name="Generic", path2_name="Current", plot_meaningfuls=False, system=1):
+    def plot_trp_comparison(self, path1, path2, generic_sensitivities, path1_name="Generic", path2_name="Current", plot_meaningfuls=False, system=1):
         # generate a plot using matplotlib from reconfig_rp, generic_rp, and time 1-8000
         x_axis = range(1, 8000)
-        print(len(self.network.reconfigures))
 
         reconfigures = []
         reconfigures_time = []
 
         for reconfig in self.network.reconfigures:
-            reconfigures_time.append(reconfig["time"])
-            reconfigures.append(path1[reconfig["time"]])
+            if len(path1) > reconfig["time"]:
+                reconfigures_time.append(reconfig["time"])
+                reconfigures.append(path1[reconfig["time"]])
 
         fig, ax = plt.subplots()
-        ax.plot(x_axis, path1, label="Reconfigurable Path")
-        ax.plot(x_axis, path2, label="Generic Path")
+        ax.plot(x_axis, path1, label=path1_name)
+        ax.plot(x_axis, path2, label=path2_name)
         
         if plot_meaningfuls:
-            ax.scatter(reconfigures_time, reconfigures, label="Meaningful", color="red")
+            ax.scatter(reconfigures_time, reconfigures, label="Meaningful", color="green")
+            
+        ax.scatter(generic_sensitivities[0], generic_sensitivities[1], label="< rx_sensitivity", color="red")
         
         ax.legend()
 
